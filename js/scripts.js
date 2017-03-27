0346c7f2d401e07a12059b1d0d5605d0405978f1
Jesse Hoare Evans and Zach Bastian <zach@>
2017-03-27 13:20:24 -0700

    create a loop to simplify jQuery alert boxes

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,27 @@
 $(document).ready(function() {
-$("h1").click(function() {
-  alert("This is a header.");
-  alert("I told you, THIS IS A HEADER!");
+
+var tags = ["h1", "p", "img"];
+
+tags.forEach(function(tag) {
+
+  $(tag).click(function(){
+  alert("This is a " + tag)}
+
+
+);
+});
 });
 
-$("p").click(function() {
-  alert("This is a paragraph.");
-});
-
-$("img").click(function() {
-  alert("This is an image.");
-});
-});
+// $("h1").click(function() {
+//   alert("This is a header.");
+//   alert("I told you, THIS IS A HEADER!");
+// });
+//
+// $("p").click(function() {
+//   alert("This is a paragraph.");
+// });
+//
+// $("img").click(function() {
+//   alert("This is an image.");
+// });
+// });
